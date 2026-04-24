beb1eeff1e76998e088c13569b6d7a299236ddf3
snuggyizme <snuggyizme@gmail.com>
2026-04-24 11:03:00 +1000

    recess

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -9,16 +9,20 @@ func _ready() -> void:
 
 func updateSprite():
 	if gun and gun.visual:
-		var instance = gun.visual.instantiate()
-		var gunSprite = instance.get_node("gun")
-		if instance.has_node("decals"):
-			for decal in instance.get_node("decals").get_children():
-				var decalSprite = Sprite2D.new()
-				decalSprite.texture = decal.texture
-				decalSprite.name = "decal (" +	gun.displayName + ") " + str(randi_range(100, 999))
-				self.add_child(decalSprite)
-		$gun.texture = gunSprite.texture
-		instance.queue_free()
+		if gun.visual.get_node("gun") is Sprite2D:
+			var instance = gun.visual.instantiate()
+			var gunSprite = instance.get_node("gun")
+			if instance.has_node("decals"):
+				for decal in instance.get_node("decals").get_children():
+					var decalSprite = Sprite2D.new()
+					decalSprite.texture = decal.texture
+					decalSprite.name = "decal (" +	gun.displayName + ") " + str(randi_range(100, 999))
+					self.add_child(decalSprite)
+			$gun.texture = gunSprite.texture
+			instance.queue_free()
+		elif gun.visual.get_node("gun") is AnimatedSprite2D:
+			var instance = gun.visual.instantiate()
+			var gunSprite = instance #aaa
 	
 func bodyEntered(body: Node2D) -> void:
 	if body.is_in_group("players") and body not in playersInRange:
